057e357e9aeb35d3f8a83cef092ab90bdbebfd22
Sepehr Norouzi <s.norouzi@medrick.info>
2023-10-23 11:03:34 +0330

    Lover - 006: Lover added. Docker bug fix.

--- lovers/models.py
+++ lovers/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User as DjangoUser
 
 
 class Lover(models.Model):
-    image = models.ImageField(upload_to='images/')
+    user = models.OneToOneField(to=DjangoUser, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='lovers/', verbose_name=_("Lover image"), null=True, blank=True)
+    profile_name = models.CharField(max_length=100, verbose_name=_("Profile name"), null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.profile_name}'
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        if self.image and hasattr(self.image, 'file'):
+            self.image.name = f"LOVER_{self.user.username}"
+
+        return super(Lover, self).save(force_insert, force_update, using, update_fields)
